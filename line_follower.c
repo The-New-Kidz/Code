2a07e7fa52edc59557b445d9432a54305b622962
thessina6535 <thessina6535@users.noreply.github.com>
2018-03-20 13:43:10 -0700

    Update line_follower.c

--- line_follower.c
+++ line_follower.c
@@ -1,35 +1,31 @@
 /*--------------------------------------------------------------------------------------------------------*\
 |*                                                                                                        *|
-|*                                      - NXT Smooth Tank Drive -                                         *|
-|*                                            ROBOTC on NXT                                               *|
+|*                                      - EV3 Line Follower Tank -                                        *|
+|*                                            ROBOTC on EV3                                               *|
 |*                                                                                                        *|
-|*  This program allows you to drive a robot via remote control using the ROBOTC Debugger.                *|
-|*  This particular method uses "Tank Drive" where each side is controlled individually like a tank.      *|
-|*  This program also ignores low values that would cause your robot to move when the joysticks fail to   *|
-|*  return back to exact center.  You may need to play with the 'threshold' value to get it just right.   *|
+|*  This program allows a robot to follow a black line.  This particular method uses "Tank Drive" where   *|
+|*  each side is controlled individually like a tank.  You may need to play with the light threshold      *|
+|*  value to get it just right.                                                                           *|
+|*                                                                                                        *|
 |*                                                                                                        *|
 |*                                        ROBOT CONFIGURATION                                             *|
 |*    NOTES:                                                                                              *|
 |*                                                                                                        *|
 |*    MOTORS & SENSORS:                                                                                   *|
 |*    [I/O Port]              [Name]              [Type]              [Description]                       *|
-|*    Port B                  motorB              NXT                 Right motor                         *|
-|*    Port C                  motorC              NXT                 Left motor                          *|
+|*    Port B                  motorB              EV3                 Right motor                         *|
+|*    Port C                  motorC              EV3                 Left motor                          *|
+|*    Port 1                  color_sensor        EV3                 color sensor                        *|
 \*---------------------------------------------------------------------------------------------------4246-*/
 
-#include "JoystickDriver.c"
-#pragma DebuggerWindows("joystickSimple")
 
 task main()
 {
-  int threshold = 20;             /* Int 'threshold' will allow us to ignore low       */
-                                  /* readings that keep our robot in perpetual motion. */
+  
 
   while(true)                            // Infinite loop:
   {
-    getJoystickSettings(joystick);
-    if(abs(joystick.joy1_y2) > threshold)   // If the right analog stick's Y-axis readings are either above or below the threshold:
-    {
+    
       motor[motorB] = joystick.joy1_y2;         // Motor B is assigned a power level equal to the right analog stick's Y-axis reading.
     }
     else                                    // Else if the readings are within the threshold:
